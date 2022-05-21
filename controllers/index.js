43e446a8412139088919b11355ace5455011daf5
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-21 10:01:03 +0200

    - Added - processor and listing to controllers.

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,3 @@
 export {default as accountController} from "./account.js";
+export {default as processorController} from "./processor.js";
+export {default as listingController} from "./listing.js";

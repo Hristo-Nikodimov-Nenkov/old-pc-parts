d9b242cc5c7317fc5b8493f714abdca8d1f0389d
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-21 10:03:29 +0200

    - Changed - development database name.

--- configs/database.js
+++ configs/database.js
@@ -1,4 +1,4 @@
-export const developmentDatabaseName = "my-database";
+export const developmentDatabaseName = "old-pc-parts-db";
 export const developmentConnectionString = `mongodb://localhost/${developmentDatabaseName}`;
 export const connectionString =
    process.env.DB_CONNECTION_STRING || developmentConnectionString;

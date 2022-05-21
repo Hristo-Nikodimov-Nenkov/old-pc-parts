ad6b974e6608481aab32b969a76444ce170071d1
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-21 09:59:37 +0200

    - Added - listing and processor services.

--- services/index.js
+++ services/index.js
@@ -1,3 +1,6 @@
-export {default as accountService} from "./account.js";
 export {default as securityService} from "./security.js";
 export {default as serviceErrors} from "./errors";
+
+export {default as accountService} from "./account.js";
+export {default as listingService} from "./listing.js";
+export {default as processorService} from "./processor.js";

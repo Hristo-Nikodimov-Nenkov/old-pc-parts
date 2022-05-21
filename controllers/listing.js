10bd796c90c2cdeec33be8f8dca7be3dfbddf70c
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-21 11:26:58 +0200

    - Added - listing dummy methods to controller.

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,1 +1,35 @@
-export default {}
+export const getListing = (req, res) => {
+   const id = req.params.id;
+
+   if (id) {
+      res.status(200).json(`Listing with id: ${id}.`);
+      return;
+   }
+
+   res.status(200).json("All listings.");
+}
+
+export const addListing = (req, res) => {
+   res.status(201).json("Listing added.");
+}
+
+export const updateListing = (req, res) => {
+   const id = req.params.id;
+
+   res.status(200).json(`Listing with id: ${id} updated.`);
+}
+
+export const removeListing = (req, res) => {
+   const id = req.params.id;
+
+   res.status(200).json(`Listing with id: ${id} removed.`);
+}
+
+const listingController = {
+   getListing,
+   addListing,
+   updateListing,
+   removeListing
+}
+
+export default listingController;

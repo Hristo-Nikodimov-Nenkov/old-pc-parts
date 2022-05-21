7553d844ec2c7e8c55333fe92229ac5eed6cfe27
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-21 11:15:01 +0200

    - Added - processor routes without validations.

--- routers/processor.js
+++ routers/processor.js
@@ -1,5 +1,11 @@
 import {Router} from "express";
+import {processorController} from "../controllers/index.js";
 
 const processorRouter = Router();
+
+processorRouter.get("/:id?", processorController.getProcessor);
+processorRouter.post("/", processorController.addProcessor);
+processorRouter.put("/:id", processorController.updateProcessor);
+processorRouter.delete("/id", processorController.removeProcessor);
 
 export default processorRouter;

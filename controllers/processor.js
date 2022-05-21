5685a0c5f617864e61db6d42803af0f1320df89e
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-21 11:12:42 +0200

    - Added - dummy methods to processor controller.

--- controllers/processor.js
+++ controllers/processor.js
@@ -1,4 +1,33 @@
+export const getProcessor = (req, res) => {
+   const id = req.params.id;
+   if (id) {
+      res.status(200).json(`Processor with id: ${id}.`);
+      return;
+   }
+
+   res.status(200).json("All processors");
+}
+
+export const addProcessor = (req, res) => {
+   res.status(201).json("Processor added.");
+}
+
+export const updateProcessor = (req, res) => {
+   const id = req.params.id;
+   res.status(200).json(`Processor with id: ${id} updated.`);
+}
+
+export const removeProcessor = (req, res) => {
+   const id = req.params.id;
+   res.status(200).json(`Processor with id: ${id} removed.`);
+}
+
+
 const processorController = {
+   getProcessor,
+   addProcessor,
+   updateProcessor,
+   removeProcessor
 }
 
 export default processorController;

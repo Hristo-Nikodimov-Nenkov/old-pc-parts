f2d2b671ae65ab99ef7d2af9e3af8aea3127de7c
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-21 10:04:37 +0200

    - Added - processor and listing to mappers.

--- mappers/index.js
+++ mappers/index.js
@@ -1,7 +1,13 @@
 export {default as accountMappers} from "./account.js";
+export {default as listingMappers} from "./listing.js";
+export {default as processorMappers} from "./processor.js";
 
 import accountMappers from "./account.js";
+import listingMappers from "./listing.js";
+import processorMappers from "./processor.js";
 
 export default {
-   accountMappers
+   accountMappers,
+   listingMappers,
+   processorMappers
 }

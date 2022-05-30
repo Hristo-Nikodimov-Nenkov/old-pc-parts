244eca729895679858d4a93ecf13951e4c2ce73d
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-30 21:47:03 +0200

    .

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
 export {default as accountController} from "./account.js";
+export {default as listingController} from "./listing.js";
 export {default as processorController} from "./processor.js";
 export {default as processorListingController} from "./processorListing.js";
-export {default as listingController} from "./listing.js";

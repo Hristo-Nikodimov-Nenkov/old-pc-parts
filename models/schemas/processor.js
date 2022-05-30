1b7b0fec221d620ce408cecb3031bc32a19f12e7
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-30 21:51:32 +0200

    - Added - processorSchema

--- models/schemas/processor.js
+++ models/schemas/processor.js
@@ -1,7 +1,28 @@
 import mongoose from "mongoose";
 
 const processorSchema = new mongoose.Schema({
-
-});
+   manufacturer: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   family: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   model: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   modelNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+   },
+   specifications: mongoose.Schema.Types.Mixed
+})
 
 export default processorSchema;

996a509417e55523ec5b62585560f0b5a94290c8
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-30 22:29:47 +0200

    - Added - Header.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import {Routes, Route} from "react-router-dom";
 import {Home} from "./components/pages";
+import {Header} from "./components/common";
 import './App.css';
 
 function App() {
-  return <Routes>
-    <Route index element={<Home/>}/>
-  </Routes>
+   return <>
+      <Header/>
+      <Routes>
+         <Route index element={<Home/>}/>
+      </Routes>
+   </>
 }
 
 export default App;

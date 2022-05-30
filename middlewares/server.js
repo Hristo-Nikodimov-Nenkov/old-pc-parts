0c625edd15c2b61f404d3b7ca073fdbf4cb8f659
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-30 21:49:07 +0200

    - Changed - application started message.

--- middlewares/server.js
+++ middlewares/server.js
@@ -1,5 +1,5 @@
-import {baseUrl, port} from "../configs/application.js";
+import {port} from "../configs/application.js";
 
 export default function (app) {
-   app.listen(port, () => console.log(`Application running on url: ${baseUrl}:${port}.`));
+   app.listen(port, () => console.log("Application started."));
 }

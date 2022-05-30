14ee054e35459da07a6840f39db9489849f7bf74
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-30 21:46:04 +0200

    - Added - processors and motherboards seeding when NODE_ENV is SEED.

--- middlewares/database.js
+++ middlewares/database.js
@@ -2,16 +2,22 @@ import mongoose from "mongoose";
 import databaseConfigs, {
    developmentConnectionString,
 } from "../configs/database.js";
+import {seedMotherboards, seedProcessors} from "../services/seed/index.js";
 
 export default function () {
    mongoose
       .connect(databaseConfigs.connectionString)
-      .then(() => {
+      .then(async () => {
          const dbEnvStr =
             databaseConfigs.connectionString === developmentConnectionString
                ? "Development"
                : "Production";
          console.log(`${dbEnvStr} database connected.`);
+
+         if (process.env.NODE_ENV?.toLowerCase() === "seed") {
+            await seedProcessors();
+            await seedMotherboards();
+         }
       })
       .catch((err) => console.log(err));
 }

5e371c4289af4abfe9258caa448593b6c7a251d2
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-31 15:59:56 +0200

    - Added - NotFound route to Routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import {Routes, Route} from "react-router-dom";
-import {About, Home} from "./components/pages";
+import {About, Home, NotFound} from "./components/pages";
 import {Footer, Header} from "./components/common";
 import './App.css';
 
 function App() {
    return <>
       <Header/>
-      <Routes className="content">
+      <Routes>
          <Route index element={<Home/>}/>
          <Route path="/about" element={<About/>}/>
+
+         <Route path="*" element={<NotFound/>}/>
       </Routes>
       <Footer/>
    </>

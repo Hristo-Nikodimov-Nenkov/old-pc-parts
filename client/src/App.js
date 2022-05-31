4376c1e249a95b83412e0a57168d825abb1b1066
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-05-31 09:33:37 +0200

    - Added - About to Routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import {Routes, Route} from "react-router-dom";
-import {Home} from "./components/pages";
+import {About, Home} from "./components/pages";
 import {Footer, Header} from "./components/common";
 import './App.css';
 
 function App() {
    return <>
       <Header/>
-      <Routes>
+      <Routes className="content">
          <Route index element={<Home/>}/>
+         <Route path="/about" element={<About/>}/>
       </Routes>
       <Footer/>
    </>

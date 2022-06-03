17ab74a1280e8622bc8d92fcc8b736f36ea86219
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-06-03 08:51:27 +0200

    - Changed - to separate Routes in routes components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import {Routes, Route} from "react-router-dom";
-import {About, Home, NotFound} from "./components/pages";
-import {Footer, Header} from "./components/common";
 import './App.css';
+import {Header, Footer} from "./components/common";
+import {Home, About, Account, Parts, NotFound} from "./components/pages";
 
 function App() {
    return <>
       <Header/>
-      <Routes>
-         <Route index element={<Home/>}/>
-         <Route path="/about" element={<About/>}/>
-
-         <Route path="*" element={<NotFound/>}/>
-      </Routes>
+      <main className="content grow">
+         <Routes>
+            <Route index element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/account/*" element={<Account/>}/>
+            <Route path="/parts/*" element={<Parts/>}/>
+            <Route path="*" element={<NotFound/>}/>
+         </Routes>
+      </main>
       <Footer/>
    </>
 }

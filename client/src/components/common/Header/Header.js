bdd4ea68d4fd9d3326d04420c35cccaa2737e132
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-06-03 08:38:45 +0200

    - Added - UserStateLinks.

--- client/src/components/common/Header/Header.js
+++ client/src/components/common/Header/Header.js
@@ -1,11 +1,14 @@
 import {Navigation} from "../Navigation";
+import {UserStateLinks} from "../UserStateLinks";
+import "./Header.css";
 
 const Header = () => {
-   return <header>
+   return <header id="app-header">
       <div id="header-logo">
          <img src="" alt="Application Logo"/>
       </div>
-      <Navigation />
+      <Navigation/>
+      <UserStateLinks/>
    </header>
 }
 

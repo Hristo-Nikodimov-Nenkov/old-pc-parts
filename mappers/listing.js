07eade751cf8a3c9bdd9db4b07114ff3006386f0
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-06-03 12:38:33 +0200

    - Added - getCreateModel, getUpdateModel, getRemoveModel, getServiceModel & getViewModel mappings.

--- mappers/listing.js
+++ mappers/listing.js
@@ -1,1 +1,87 @@
-export default {}
+export const getCreateModel = (partId, model) => {
+   const {
+      price,
+      shipping,
+      includesTaxes,
+      link
+   } = model;
+
+   return {
+      partId,
+      price,
+      shipping,
+      includesTaxes,
+      link
+   }
+}
+
+export const getUpdateModel = (model) => {
+   const {
+      partId,
+      listingId,
+      price,
+      shipping,
+      includesTaxes,
+      link
+   } = model;
+
+   return {
+      partId,
+      listingId,
+      price,
+      shipping,
+      includesTaxes,
+      link
+   }
+}
+
+export const getRemoveModel = (model) => {
+   const {
+      userId,
+      partId,
+      listingId
+   } = model;
+
+   return {
+      userId,
+      partId,
+      listingId
+   }
+}
+
+export const getServiceModel = (dbModel) => {
+   const {
+      _id,
+      partId,
+      price,
+      shipping,
+      includesTaxes,
+      link
+   } = dbModel;
+
+   return {
+      id: _id,
+      partId,
+      price,
+      shipping,
+      includesTaxes,
+      link
+   }
+}
+
+export const getViewModel = (dbModel) => {
+   const viewModel = getServiceModel(dbModel);
+   delete viewModel.id;
+
+   return viewModel
+}
+
+const listingMapper = {
+   getCreateModel,
+   getUpdateModel,
+   getRemoveModel,
+   getServiceModel,
+   getViewModel
+}
+
+export default listingMapper;

11e6ee635ecf8ecca61c13650236a437b7be864e
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-06-03 15:22:04 +0200

    - Added - listingService

--- services/listing.js
+++ services/listing.js
@@ -1,1 +1,16 @@
-export default {}
+import {Listing} from "../models/index.js";
+import {getViewModel} from "../mappers/listing.js";
+
+export const create = async (model) => {
+   try {
+      const listing = new Listing(model);
+      await listing.save();
+
+      return getViewModel(listing);
+   } catch (err) {
+      console.log(err.message);
+   }
+}
+
+const listingService = {}
+export default listingService;
